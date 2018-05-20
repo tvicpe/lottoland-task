64f3dc82908bf4868eac7db498bf2e2a19037833
tvicpe <tv@tomsa.md>
2018-05-20 13:28:26 +0300

    Display data of the winners component. Create a helper class result. Use bootstrap.

--- src/app/components/winners/winners.component.ts
+++ src/app/components/winners/winners.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Result} from '../../util/result';
 
 @Component({
   selector: 'app-winners',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WinnersComponent implements OnInit {
 
-  constructor() { }
+  odds: any[] = [];
+  labels = Result.labels;
+
+  @Input() set data(data: any) {
+    if (!data) {
+      return;
+    }
+    const {odds} = data;
+    if (!odds) {
+      return;
+    }
+    for (const index in odds) {
+      if (odds.hasOwnProperty(index) && index !== 'rank0') {
+        odds[index].matchLabel = this.labels[index].matchLabel;
+        odds[index].rank = this.labels[index].rank;
+        this.odds.push(odds[index]);
+      }
+    }
+
+  }
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
+
 
 }
---
+++ src/app/util/result.ts
@@ -0,0 +1,17 @@
+export class Result {
+  static labels = {
+    rank1: {matchLabel: '5 Numbers + 2 Euronumbers', rank: 'I'},
+    rank2: {matchLabel: '5 Numbers + 1 Euronumber', rank: 'II'},
+    rank3: {matchLabel: '5 Numbers + 0 Euronumbers', rank: 'III'},
+    rank4: {matchLabel: '4 Numbers + 2 Euronumbers', rank: 'IV'},
+    rank5: {matchLabel: '4 Numbers + 1 Euronumber', rank: 'V'},
+    rank6: {matchLabel: '4 Numbers + 0 Euronumber', rank: 'VI'},
+    rank7: {matchLabel: '3 Numbers + 2 Euronumbers', rank: 'VII'},
+    rank8: {matchLabel: '2 Numbers + 2 Euronumbers', rank: 'VIII'},
+    rank9: {matchLabel: '3 Numbers + 1 Euronumber', rank: 'IX'},
+    rank10: {matchLabel: '3 Numbers + 0 Euronumbers', rank: 'X'},
+    rank11: {matchLabel: '1 Number + 2 Euronumbers', rank: 'XI'},
+    rank12: {matchLabel: '2 Numbers + 1 Euronumber', rank: 'XII'},
+  };
+
+}
